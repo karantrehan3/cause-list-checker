dfc4772b66ed0e4fdf03a1e8face6e83b57f46d5
Karan Trehan <karan.trehan@springworks.in>
2024-12-27 20:21:56 +0530

    Adding Email Templates and Support

--- app/pdf_searcher.py
+++ app/pdf_searcher.py
@@ -1,30 +1,45 @@
 import PyPDF2
 import requests
 from io import BytesIO
-from typing import List, Tuple
+import time
+import random
+from typing import List, Dict
 
 
 class PDFSearcher:
     def __init__(self, search_term: str):
         self.search_term = search_term
 
-    def search_pdf(self, pdf_url: str) -> Tuple[bool, List[int]]:
-        # Fetch the PDF content
-        response = requests.get(pdf_url)
+    def search_pdf(self, pdfs: List[Dict[str, str]]) -> List[Dict[str, any]]:
+        results = []
+        for pdf in pdfs:
+            # Add a random delay between 0.5 and 1 seconds
+            time.sleep(random.uniform(0.5, 1))
 
-        # Log the headers of the response
-        if response.status_code != 200:
-            return False, []
+            pdf_name = pdf["pdf_name"]
+            pdf_url = pdf["pdf_url"]
 
-        # Read the PDF from memory
-        pdf_file = BytesIO(response.content)
-        reader = PyPDF2.PdfReader(pdf_file)
+            # Fetch the PDF content
+            response = requests.get(pdf_url)
 
-        # Search for the term in each page
-        found_pages = []
-        for page_num, page in enumerate(reader.pages):
-            text = page.extract_text()
-            if text and self.search_term.lower() in text.lower():
-                found_pages.append(page_num + 1)  # Page numbers are 1-based
+            # Log the headers of the response
+            if response.status_code != 200:
+                continue
 
-        return bool(found_pages), found_pages
+            # Read the PDF from memory
+            pdf_file = BytesIO(response.content)
+            reader = PyPDF2.PdfReader(pdf_file)
+
+            # Search for the term in each page
+            found_pages = []
+            for page_num, page in enumerate(reader.pages):
+                text = page.extract_text()
+                if text and self.search_term.lower() in text.lower():
+                    found_pages.append(page_num + 1)  # Page numbers are 1-based
+
+            if found_pages:
+                results.append(
+                    {"pdf_name": pdf_name, "pdf_url": pdf_url, "page_nums": found_pages}
+                )
+
+        return results
