eb1fd08b23fe1c759522d7ea9e8b5faa2966c027
Karan Trehan <karan.trehan@springworks.in>
2024-12-28 18:32:33 +0530

    Making the Search Cause List Flow async

--- app/routes/search/cause_list/controllers.py
+++ app/routes/search/cause_list/controllers.py
@@ -17,48 +17,63 @@ async def scrape_search_and_notify(
     date: Optional[str] = None,
     recipient_emails: Optional[List[str]] = None,
 ) -> Dict[str, Any]:
+    if search_lock.locked():
+        raise HTTPException(status_code=429, detail="Too Many Requests")
+
     if not recipient_emails:
         recipient_emails = settings.EMAIL_RECIPIENTS.split(",")
 
     if not date:
         date = (datetime.now() + timedelta(days=1)).strftime("%d/%m/%Y")
 
-    scraper = Scraper()
-    searcher = PDFSearcher(search_term=search_term)
-    emailer = Emailer()
-    error_handler = ErrorHandler(emailer, recipient_emails)
+    async def process_search_and_notify():
+        scraper = Scraper()
+        searcher = PDFSearcher(search_term=search_term)
+        emailer = Emailer()
+        error_handler = ErrorHandler(emailer, recipient_emails)
 
-    # Try to acquire the lock
-    if not search_lock.locked():
         async with search_lock:
             try:
                 # Step 1: Scrape the page and get PDF links
                 pdfs = scraper.parse_table_and_download_pdfs(date)
                 if not pdfs:
                     send_email(emailer, recipient_emails, search_term, date, pdfs, [])
-                    return {"message": "No Cause Lists found"}
+                    print("ALERT! No Cause Lists found", flush=True)
+                    return
+
+                print(
+                    "PROGRESS! Cause Lists found: ",
+                    json.dumps(pdfs, indent=2),
+                    flush=True,
+                )
 
                 # Step 2: Search for the term in the PDFs (run in separate thread)
                 results = await asyncio.to_thread(searcher.search_pdf, pdfs)
 
-                print("Cause List Search Results: ", json.dumps(results, indent=4))
+                print(
+                    "PROGRESS! Cause List Search Results: ",
+                    json.dumps(results, indent=2),
+                    flush=True,
+                )
 
                 # Step 3: If results found, send an email notification
                 send_email(emailer, recipient_emails, search_term, date, pdfs, results)
 
-                return {
-                    "message": "Search completed and email sent!",
-                    "results": results,
-                }
+                print(
+                    "SUCCESS! Search completed and email sent! ",
+                    json.dumps(results, indent=2),
+                    flush=True,
+                )
+                return
 
             except Exception as e:
                 error_message, stack_trace = error_handler.handle_exception(
                     e, {"search_term": search_term, "date": date}
                 )
                 raise HTTPException(status_code=500, detail=error_message)
-    else:
-        # If lock is acquired, respond with 429
-        raise HTTPException(status_code=429, detail="Too Many Requests")
+
+    asyncio.create_task(process_search_and_notify())
+    return {"message": "Search and notification process started"}
 
 
 def send_email(
